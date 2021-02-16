5dd163b32cafadbed24152a9d67a964aeed3e939
sumancoder <76449399+sumancoder@users.noreply.github.com>
2021-02-16 22:28:07 +0530

    Update SpringFoxConfig.java

--- autosearch/src/main/java/com/autosearch/complete/config/SpringFoxConfig.java
+++ autosearch/src/main/java/com/autosearch/complete/config/SpringFoxConfig.java
@@ -6,8 +6,10 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
 
 @Configuration
+@EnableSwagger2WebFlux
 public class SpringFoxConfig {                                    
     @Bean
     public Docket api() { 
